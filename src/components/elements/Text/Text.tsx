4b9f26fd5c103cbf24020ff2aeb744ebbe83c1df
MariaBoi <akivene55@gmail.com>
2022-05-16 21:23:05 +0300

    added classes function

--- src/components/elements/Text/Text.tsx
+++ src/components/elements/Text/Text.tsx
@@ -1,21 +1,44 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, useCallback } from "react";
 import styles from "./Text.module.scss";
 
 interface TextProps {
     children: ReactNode;
     isParagraph?: boolean;
-    textType: "textBig" | "textMedium" | "textSmall";
+    textType?: "big" | "medium" | "small";
     color?: "dark-text-color" | "light-text-color";
+    bold?: boolean;
 }
 
-export const Text: FC<TextProps> = ({ children, isParagraph, textType, color }) => {
+export const Text: FC<TextProps> = ({
+    children,
+    isParagraph,
+    textType = "medium",
+    color = "dark-text-color",
+    bold = false,
+}) => {
+    const getClassnames = useCallback((obj: { [key: string]: boolean }): string => {
+        return Object.entries(obj)
+            .filter(arr => arr[1])
+            .map(arr => arr[0])
+            .join(" ");
+    }, []);
+
+    const TextClasses = getClassnames({
+        general: true,
+        [styles[textType]]: true,
+        bold,
+        [color]: true,
+    });
+
     if (isParagraph) {
-        return <p className={`general ${styles[textType]} ${color}`}>{children}</p>;
+        return <p className={TextClasses}>{children}</p>;
     }
-    return <span className={`general ${styles[textType]} ${color}`}>{children}</span>;
+    return <span className={TextClasses}>{children}</span>;
 };
 
 Text.defaultProps = {
     isParagraph: false,
     color: "dark-text-color",
+    bold: false,
+    textType: "medium",
 };
