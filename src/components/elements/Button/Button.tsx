2da3016799a984c4014a1c1da72cdcc0744f9f66
MariaBoi <akivene55@gmail.com>
2022-05-16 12:50:54 +0300

    added width prop and temporary index

--- src/components/elements/Button/Button.tsx
+++ src/components/elements/Button/Button.tsx
@@ -1,20 +1,34 @@
-import React, { MouseEvent } from "react";
+import React, { ReactNode } from "react";
+import Link from "next/link";
 import styles from "./Button.module.scss";
 
 type ButtonProps = {
-    text: string;
-    type?: "button" | "submit" | "reset";
-    onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+    children?: ReactNode;
+    link?: string;
+    width?: "width320" | "width248";
+    type?: "button" | "submit";
+    onClick: () => void;
 };
 
-export const Button: React.FC<ButtonProps> = ({ text, type, onClick }) => {
+export const Button: React.FC<ButtonProps> = ({ children, type = "button", onClick, width = "width320", link }) => {
+    if (link) {
+        <Link href={link}>
+            <button className={`${styles.button} ${styles[width]}`} onClick={onClick} type={type ? "button" : "button"}>
+                {children}
+            </button>
+        </Link>;
+    }
+
     return (
-        <button className={styles.button} onClick={onClick} type={type}>
-            {text}
+        <button className={`${styles.button} ${styles[width]}`} onClick={onClick} type="button">
+            {children}
         </button>
     );
 };
 
 Button.defaultProps = {
     type: "button",
+    width: "width320",
+    children: "",
+    link: "",
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from "next";
 
 import { DefaultLayout } from "@layouts/DefaultLayout";
+import { Button } from "../components/elements/Button/Button";
 
 const HomePage: NextPage = () => {
-    return <DefaultLayout>Home</DefaultLayout>;
+    return (
+        <DefaultLayout>
+            Home{" "}
+            <Button onClick={() => {}} text="увыйти" width="width248">
+                buu
+            </Button>
+            <Button onClick={() => {}} text="увыйти" link="/hhh">
+                link
+            </Button>
+        </DefaultLayout>
+    );
 };
 
 export default HomePage;
