56c039755342fb435b0dd70544d7f278c268cbb5
MariaBoi <akivene55@gmail.com>
2022-05-21 16:20:30 +0300

    created get ready block

---
+++ src/components/modules/GetReadyBlock/GetReadyBlock.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Title } from "@elements/Title/Title";
+import { Text } from "@elements/Text/Text";
+import Image from "next/image";
+import styles from "./GetReadyBlock.module.scss";
+
+export const GetReadyBlock = () => {
+    return (
+        <section className={styles.section}>
+            <div className={styles.wrapper}>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="/assets/images/getReadyToInterview.png"
+                        layout="fill"
+                        objectFit="cover"
+                        objectPosition="top"
+                        alt="get ready to interview bgd"
+                    />
+                </div>
+                <div className={styles.content}>
+                    <Title level={1}>Підготуйся до сіпвбесіди!</Title>
+                    <Text textType="big" isParagraph>
+                        Платформа яка допоможе підготовки до технічної співбесіди в міжнародну компанію
+                    </Text>
+                    <button type="button">Розпочати</button>
+                </div>
+            </div>
+        </section>
+    );
+};
---
+++ src/components/modules/GetReadyBlock/index.ts
@@ -0,0 +1,1 @@
+export { GetReadyBlock } from "./GetReadyBlock";
