a21394c860aec6a7a54b80edabdd565bb6c665d5
MariaBoi <akivene55@gmail.com>
2022-05-25 11:42:05 +0300

    removed onclick

--- src/components/elements/Button/ButtonLink.tsx
+++ src/components/elements/Button/ButtonLink.tsx
@@ -1,20 +1,15 @@
-import React, { MouseEventHandler, ReactNode } from "react";
-/* eslint  "jsx-a11y/anchor-is-valid": 0  
-  "jsx-a11y/click-events-have-key-events": "off" ,
-  "jsx-a11y/no-static-element-interactions": "off" 
- */
+import React, { ReactNode } from "react";
+
 type ButtonLinkProps = {
     className: string;
     children: ReactNode;
-    onClick?: MouseEventHandler<HTMLAnchorElement>;
+    href: string;
 };
 
-export const ButtonLink = React.forwardRef<HTMLAnchorElement, ButtonLinkProps>(
-    ({ className, children, onClick }, ref) => {
-        return (
-            <a ref={ref} className={className} onClick={onClick}>
-                {children}
-            </a>
-        );
-    }
-);
+export const ButtonLink = React.forwardRef<HTMLAnchorElement, ButtonLinkProps>(({ className, children, href }, ref) => {
+    return (
+        <a ref={ref} className={className} href={href}>
+            {children}
+        </a>
+    );
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
 import type { NextPage } from "next";
 
 import { DefaultLayout } from "@layouts/DefaultLayout";
-import { Button } from "../components/elements/Button/Button";
 
 const HomePage: NextPage = () => {
-    return (
-        <DefaultLayout>
-            <Button color="grey" link="ygjy" size="small">
-                ckfxjd
-            </Button>
-            <Button link="zdfv'ZD:">ckfxjd</Button>
-            <Button>ckfxjd</Button>Home
-        </DefaultLayout>
-    );
+    return <DefaultLayout>Home</DefaultLayout>;
 };
 
 export default HomePage;
