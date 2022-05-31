fe4b5d2d397b581d041c5124fdc119c466200fb0
MariaBoi <akivene55@gmail.com>
2022-05-31 22:53:04 +0300

    added alarm

--- src/components/elements/LoadBar/LoadBar.tsx
+++ src/components/elements/LoadBar/LoadBar.tsx
@@ -1,3 +1,4 @@
+import { Icon } from "@elements/Icon";
 import { Text } from "@elements/Text";
 import React, { FC } from "react";
 import styles from "./LoadBar.module.scss";
@@ -5,21 +6,21 @@ import styles from "./LoadBar.module.scss";
 interface LoadBarProps {
     currentQuestionNumber: number;
     numberOfQuestions: number;
+    onClose: () => void;
 }
 
-export const LoadBar: FC<LoadBarProps> = ({ currentQuestionNumber, numberOfQuestions }) => {
-    const onTestClose = () => {};
-
+export const LoadBar: FC<LoadBarProps> = ({ onClose, currentQuestionNumber, numberOfQuestions }) => {
     return (
         <div className={styles.wrapper}>
-            <div className={styles.clockWrapper} />
-
+            <div className={styles.alarm}>
+                <Icon name="alarm" />
+            </div>
             <Text size="small">
                 {currentQuestionNumber}/{numberOfQuestions}
             </Text>
             <progress className={styles.progress} max="1" value={currentQuestionNumber / numberOfQuestions} />
 
-            <button type="button" className={styles.closeContainer} onClick={onTestClose}>
+            <button type="button" className={styles.closeContainer} onClick={onClose}>
                 <div className={styles.leftRight} />
                 <div className={styles.rightLeft} />
             </button>
