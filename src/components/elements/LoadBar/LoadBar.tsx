4b5f093068c00f16de188f9f1802d206c873bfde
MariaBoi <akivene55@gmail.com>
2022-05-31 10:06:43 +0300

    created loadbar

---
+++ src/components/elements/LoadBar/LoadBar.tsx
@@ -0,0 +1,30 @@
+import { Text } from "@elements/Text";
+import React, { FC } from "react";
+import styles from "./LoadBar.module.scss";
+
+interface LoadBarProps {
+    currentQuestionNumber: number;
+    numberOfQuestions: number;
+}
+
+const LoadBar: FC<LoadBarProps> = ({ currentQuestionNumber, numberOfQuestions }) => {
+    const onTestClose = () => {};
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.clockWrapper} />
+
+            <Text size="small">
+                {currentQuestionNumber}/{numberOfQuestions}
+            </Text>
+            <progress className={styles.progress} max="1" value={currentQuestionNumber / numberOfQuestions} />
+
+            <button type="button" className={styles.closeContainer} onClick={onTestClose}>
+                <div className={styles.leftRight} />
+                <div className={styles.rightLeft} />
+            </button>
+        </div>
+    );
+};
+
+export default LoadBar;
