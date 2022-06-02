dcfdc143ba23c29fb31fd5ae3796eaff8842aff2
MariaBoi <akivene55@gmail.com>
2022-06-02 22:28:30 +0300

    refactored path

--- src/components/elements/Breadcrumb/Breadcrumb.tsx
+++ src/components/elements/Breadcrumb/Breadcrumb.tsx
@@ -14,11 +14,8 @@ export const Breadcrumb = () => {
     const [breadcrumbs, setBreadcrumbs] = useState<LinkType[]>([]);
 
     useEffect(() => {
-        const pathWithoutQuery = router.asPath.split("?")[0];
-        let pathArray = pathWithoutQuery.split("/");
-        pathArray.shift();
-
-        pathArray = pathArray.filter(path => path !== "");
+        const pathArray = router.asPath.split("/").filter(path => path !== "");
+        console.log(pathArray);
 
         const links = pathArray.map((path, index) => {
             const link = `/${pathArray.slice(0, index + 1).join("/")}`;
@@ -38,17 +35,16 @@ export const Breadcrumb = () => {
                         <Icon name="home" width={18} height={17} />
                     </IconLink>
                 </li>
-                {breadcrumbs &&
-                    breadcrumbs.map(item => (
-                        <>
-                            <Icon name="arrowRight" width={5} height={8} />
-                            <li className={styles.linkItem}>
-                                <Text size="small" color="grey-text-color">
-                                    <Link href={item.link}>{item.name}</Link>
-                                </Text>
-                            </li>
-                        </>
-                    ))}
+                {breadcrumbs.map(item => (
+                    <>
+                        <Icon name="arrowRight" width={5} height={8} />
+                        <li className={styles.linkItem}>
+                            <Text size="small" color="grey-text-color">
+                                <Link href={item.link}>{item.name}</Link>
+                            </Text>
+                        </li>
+                    </>
+                ))}
             </ul>
         </div>
     );
