63aed626df403c290af7f9ccbbcd15134a93985a
MariaBoi <akivene55@gmail.com>
2022-06-15 10:07:56 +0300

    made linksarray mandatory

--- src/components/elements/Breadcrumb/Breadcrumb.tsx
+++ src/components/elements/Breadcrumb/Breadcrumb.tsx
@@ -1,32 +1,16 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { Icon } from "../Icon/Icon";
-
 import styles from "./Breadcrumb.module.scss";
 import { IconLink } from "./IconLink";
 import { Text } from "../Text/Text";
-import { parseUrl, LinkType } from "../../../utils/parseUrl";
+import { LinkType } from "../../../utils/parseUrl";
 
 interface BreadcrumbProps {
-    linksArray?: LinkType[];
-    captions: string[];
+    linksArray: LinkType[];
 }
 
-export const Breadcrumb: FC<BreadcrumbProps> = ({ linksArray, captions }) => {
-    const router = useRouter();
-    const [breadcrumbs, setBreadcrumbs] = useState<LinkType[]>([]);
-
-    useEffect(() => {
-        if (!linksArray) {
-            const urlString = router.asPath;
-            const pathArray = parseUrl(urlString);
-            setBreadcrumbs(pathArray);
-        } else {
-            setBreadcrumbs(linksArray);
-        }
-    }, [router.asPath, linksArray]);
-
+export const Breadcrumb: FC<BreadcrumbProps> = ({ linksArray }) => {
     return (
         <div className={styles.wrapper}>
             <ul className={styles.list}>
@@ -35,12 +19,12 @@ export const Breadcrumb: FC<BreadcrumbProps> = ({ linksArray, captions }) => {
                         <Icon name="home" width={18} height={17} />
                     </IconLink>
                 </li>
-                {breadcrumbs.map((item, index) => (
+                {linksArray.map(item => (
                     <>
                         <Icon name="arrowRight" width={5} height={8} />
                         <li className={styles.linkItem}>
                             <Text size="small" color="grey-text-color">
-                                <Link href={item.link}>{captions[index] ?? item.name}</Link>
+                                <Link href={item.link}>{item.name}</Link>
                             </Text>
                         </li>
                     </>
