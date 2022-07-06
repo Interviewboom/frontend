eaa9ae2830f3a86c87d75859e013f758764d5709
MariaBoi <akivene55@gmail.com>
2022-07-06 13:15:08 +0300

    fixed type

--- src/api/stringifyParams.ts
+++ src/api/stringifyParams.ts
@@ -1,8 +1,12 @@
 import { URLSearchParams } from "url";
 
-export interface paramsType {
-    [k: string]: string | number;
-}
+export type paramsType =
+    | string
+    | URLSearchParams
+    | Record<string, string | readonly string[]>
+    | Iterable<[string, string]>
+    | readonly [string, string][]
+    | undefined;
 
 export const stringifyParams = (obj: paramsType) => {
     return `?${new URLSearchParams(obj)}`;
