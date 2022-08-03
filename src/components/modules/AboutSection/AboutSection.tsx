a9f1f1960644dbecc0c2dd100a5aabdacb0e114a
MariaBoi <akivene55@gmail.com>
2022-08-03 23:14:35 +0300

    changed mobile verdion in about section

--- src/components/modules/AboutSection/AboutSection.tsx
+++ src/components/modules/AboutSection/AboutSection.tsx
@@ -10,27 +10,29 @@ export const AboutSection = () => {
     return (
         <section className={styles.section}>
             <div className={styles.wrapper}>
-                <div className={styles.text}>
-                    <div className={styles.titleWrapper}>
-                        <Title level={2} className={styles.titleMargin}>
-                            About
-                        </Title>
+                {/* <div className={styles.text}> */}
+                <div className={styles.titleWrapper}>
+                    <Title level={2} className={styles.titleMargin}>
+                        About
+                    </Title>
+                    <div className={styles.logo}>
                         <Logo />
                     </div>
-
-                    <Text isParagraph className={styles.pMargin}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of
-                        type and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                        also the leap into electronic typesetting, remaining essentially unchanged.
-                    </Text>
-                    <Text isParagraph>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of
-                        type and scrambled it to make a type specimen book. It has survived not only five centuries, but
-                        also the leap into electronic typesetting, remaining essentially unchanged.
-                    </Text>
                 </div>
+
+                <Text isParagraph className={styles.pMargin}>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                    leap into electronic typesetting, remaining essentially unchanged.
+                </Text>
+                <Text isParagraph>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                    leap into electronic typesetting, remaining essentially unchanged.
+                </Text>
+                {/* </div> */}
                 <div className={styles.image}>
                     <Image src="/assets/images/about.png" alt="laptop" width={736} height={356} />
                 </div>
