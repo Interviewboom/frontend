b9dd9853e6f0000f05bdbaa9be6221f223b34e1d
Karina Barinova <kbarinova11@gmail.com>
2023-05-04 10:57:14 +0100

    Format nav links taking into account anchors (#8)

--- src/components/modules/Header/Navigation.tsx
+++ src/components/modules/Header/Navigation.tsx
@@ -1,15 +1,23 @@
+import { FC } from "react";
 import Link from "next/link";
+import { formatLink } from "@utils/formatLink";
+
 import { NAVIGATION_ITEMS } from "./const";
 
 import styles from "./Navigation.module.scss";
 
-export const Navigation = () => {
+interface NavigationItem {
+    name: string;
+    link: string;
+}
+
+export const Navigation: FC = () => {
     return (
         <nav className={styles.navigation}>
             <ul className={styles.navigationList}>
-                {NAVIGATION_ITEMS.map(item => (
+                {NAVIGATION_ITEMS.map((item: NavigationItem) => (
                     <li key={item.name} className={styles.navigationItem}>
-                        <Link legacyBehavior href={item.link}>
+                        <Link legacyBehavior href={formatLink(item.link)}>
                             {item.name}
                         </Link>
                     </li>
