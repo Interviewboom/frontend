ca58f0bb5bb8d6dfc23c961d9681c1609e99d60b
Flowerinno <akellastoopi@gmail.com>
2023-05-16 12:42:19 +0300

    redesign frontGreetingSection, create mobile layout

--- src/components/modules/FrontGreetingSection/FrontGreetingSection.tsx
+++ src/components/modules/FrontGreetingSection/FrontGreetingSection.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+
 import { Title } from "@elements/Title/Title";
 import { Text } from "@elements/Text/Text";
-import Image from "next/legacy/image";
+import Image from "next/image";
 import { Button } from "@elements/Button/Button";
+import { Icon } from "@elements/Icon";
 
 import styles from "./FrontGreetingSection.module.scss";
 
@@ -10,25 +12,50 @@ export const FrontGreetingSection = () => {
     return (
         <section className={styles.section}>
             <div className={styles.wrapper}>
-                <div className={styles.imageContainer}>
-                    <div className={styles.gradient} />
-                    <Image
-                        src="/assets/images/getReadyToInterview.png"
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition="top"
-                        alt="get ready to interview background"
-                        priority
-                    />
-                </div>
                 <div className={styles.content}>
-                    <Title level={1} className={styles.marginTitle}>
-                        Get ready for future interview
+                    <Title level={1} className={styles.title}>
+                        Prove Your Coding Aptitude
+                        <Image
+                            src="/assets/images/greetingSection/icon.png"
+                            width={50}
+                            height={44}
+                            alt="icon"
+                            quality={100}
+                            className={styles.imageIcon}
+                        />
                     </Title>
-                    <Text size="big" isParagraph className={styles.marginTitle}>
-                        This platform helps to prepare for a technical interview with an international company.
+                    <Text size="big" isParagraph className={styles.subtitle}>
+                        Sharpen your skills with comprehensive testing. Measure your abilities with challenges and
+                        assessments.
                     </Text>
-                    <Button link="/categories">Start</Button>
+                    <div className={styles.buttons}>
+                        <Button link="/categories" className={styles.contentButton}>
+                            Explore our tests
+                        </Button>
+                        <a href="#tests" className={styles.contentArrowButton}>
+                            <Icon name="arrowDown" className={styles.arrowDown} />
+                        </a>
+                    </div>
+
+                    <div className={styles.statistics}>
+                        <Text isParagraph className={styles.info}>
+                            Developers tested(140)
+                        </Text>
+                        <Text isParagraph className={styles.info}>
+                            Average score(0.88)
+                        </Text>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="/assets/images/greetingSection/greeting.svg"
+                        width={575}
+                        height={475}
+                        alt="roadmap"
+                        quality={100}
+                        priority
+                        className={styles.imageRoadmap}
+                    />
                 </div>
             </div>
         </section>
--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -4,7 +4,16 @@ import { REACT_APP_API_BASE_URL } from "./constants";
 
 export const api = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: REACT_APP_API_BASE_URL }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: REACT_APP_API_BASE_URL,
+        prepareHeaders: headers => {
+            headers.set(
+                "Authorization",
+                `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1vZGVsZmFrQGdtYWlsLmNvbSIsInN1YiI6MSwicm9sZXMiOlsiYWRtaW4iXSwiaWF0IjoxNjg0MjEzODQyLCJleHAiOjE2ODQzMDAyNDJ9.sRy-YwedWoVyT7oalgLsi3pXuOfXii_zpJTvH5JXhPI`
+            );
+            return headers;
+        },
+    }),
     tagTypes: ["api"],
     endpoints: () => ({}),
     // eslint-disable-next-line consistent-return
