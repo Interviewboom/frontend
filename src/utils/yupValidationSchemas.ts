9277ebe7f8b6b4bcd339b5128bfc041bb9cc8e46
Karina Barinova <kbarinova11@gmail.com>
2023-05-16 22:59:28 +0100

    Refactor

--- src/utils/yupValidationSchemas.ts
+++ src/utils/yupValidationSchemas.ts
@@ -1,35 +1,30 @@
 import * as Yup from "yup";
 import { emailPattern, passwordPattern } from "./regexConstants";
 
+const emailValidation = Yup.string()
+    .email("Invalid email address")
+    .matches(emailPattern, "Invalid email address")
+    .required("Email is required");
+
+const passwordValidation = Yup.string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .matches(passwordPattern, "Password must be strong");
+
 export const signInValidationSchema = Yup.object().shape({
-    email: Yup.string()
-        .email("Invalid email address")
-        .matches(emailPattern, "Invalid email address")
-        .required("Email is required"),
-    password: Yup.string()
-        .required("Password is required")
-        .min(8, "Password must be at least 8 characters")
-        .matches(passwordPattern, "Password must be strong"),
+    email: emailValidation,
+    password: passwordValidation,
 });
 
 export const signUpValidationSchema = Yup.object().shape({
     name: Yup.string().required(),
-    email: Yup.string()
-        .email("Invalid email address")
-        .matches(emailPattern, "Invalid email address")
-        .required("Email is required"),
-    password: Yup.string()
-        .required("Password is required")
-        .min(8, "Password must be at least 8 characters")
-        .matches(passwordPattern, "Password must be strong"),
-    repeatPassword: Yup.string()
+    email: emailValidation,
+    password: passwordValidation,
+    repeatPassword: passwordValidation
         .oneOf([Yup.ref("password")], "Passwords must match")
         .required("Please confirm your password"),
 });
 
 export const resetPasswordValidationSchema = Yup.object().shape({
-    email: Yup.string()
-        .email("Invalid email address")
-        .matches(emailPattern, "Invalid email address")
-        .required("Email is required"),
+    email: emailValidation,
 });
