ecd9dc24667e001c3bc49b0954ff527d6620c439
Karina Barinova <kbarinova11@gmail.com>
2023-05-16 22:53:59 +0100

    Move regex to a  separate file

---
+++ src/utils/regexConstants.ts
@@ -0,0 +1,3 @@
+export const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+export const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
--- src/utils/yupValidationSchemas.ts
+++ src/utils/yupValidationSchemas.ts
@@ -1,26 +1,27 @@
 import * as Yup from "yup";
+import { emailPattern, passwordPattern } from "./regexConstants";
 
 export const signInValidationSchema = Yup.object().shape({
     email: Yup.string()
         .email("Invalid email address")
-        .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, "Invalid email address")
+        .matches(emailPattern, "Invalid email address")
         .required("Email is required"),
     password: Yup.string()
         .required("Password is required")
         .min(8, "Password must be at least 8 characters")
-        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]){12}[A-Za-z\d@$!%*?&]+$/, "Password must be strong"),
+        .matches(passwordPattern, "Password must be strong"),
 });
 
 export const signUpValidationSchema = Yup.object().shape({
     name: Yup.string().required(),
     email: Yup.string()
         .email("Invalid email address")
-        .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, "Invalid email address")
+        .matches(emailPattern, "Invalid email address")
         .required("Email is required"),
     password: Yup.string()
         .required("Password is required")
         .min(8, "Password must be at least 8 characters")
-        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]){5}[A-Za-z\d@$!%*?&]+$/, "Password must be strong"),
+        .matches(passwordPattern, "Password must be strong"),
     repeatPassword: Yup.string()
         .oneOf([Yup.ref("password")], "Passwords must match")
         .required("Please confirm your password"),
@@ -29,6 +30,6 @@ export const signUpValidationSchema = Yup.object().shape({
 export const resetPasswordValidationSchema = Yup.object().shape({
     email: Yup.string()
         .email("Invalid email address")
-        .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, "Invalid email address")
+        .matches(emailPattern, "Invalid email address")
         .required("Email is required"),
 });
