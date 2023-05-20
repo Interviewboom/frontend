de6a68161680d9fe26379e91ce2cf09661628d9b
Flowerinno <akellastoopi@gmail.com>
2023-05-20 15:13:30 +0300

    layout changes

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -4,7 +4,16 @@ import { REACT_APP_API_BASE_URL } from "./constants";
 
 export const api = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({ baseUrl: REACT_APP_API_BASE_URL }),
+    baseQuery: fetchBaseQuery({
+        baseUrl: REACT_APP_API_BASE_URL,
+        prepareHeaders: headers => {
+            headers.set(
+                "Authorization",
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1vZGVsZmFrQGdtYWlsLmNvbSIsInN1YiI6MSwicm9sZXMiOlsiYWRtaW4iXSwiaWF0IjoxNjg0NTgwMzk2LCJleHAiOjE2ODQ2NjY3OTZ9.aILw-n-Zt2oyIlFCQAYOCxdw1wohe3SQHaJ7TGgdkbk"
+            );
+            return headers;
+        },
+    }),
     tagTypes: ["api"],
     endpoints: () => ({}),
     // eslint-disable-next-line consistent-return
