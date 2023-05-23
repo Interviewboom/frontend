13c8b047023c8f302882f7262349c47e008867ab
Flowerinno <akellastoopi@gmail.com>
2023-05-23 08:31:27 +0300

    fix(frontGreetingSection): rebase master features

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -8,7 +8,10 @@ export const api = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: REACT_APP_API_BASE_URL,
         prepareHeaders: headers => {
-            headers.set("Authorization", "Bearer TOKEN");
+            headers.set(
+                "Authorization",
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1vZGVsZmFrQGdtYWlsLmNvbSIsInN1YiI6MSwicm9sZXMiOlsiYWRtaW4iXSwiaWF0IjoxNjg0ODE5NjI0LCJleHAiOjE2ODQ5MDYwMjR9.wksIpcaW5kJzvVxBsMjzNq-WrcOZmVkp1jXTFiS_xWM"
+            );
             return headers;
         },
     }),
