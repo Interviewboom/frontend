c394e10dc9b4a15ae8ec9de726232e6980ad8b3a
Flowerinno <akellastoopi@gmail.com>
2023-05-23 08:50:29 +0300

    fix: remove token

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -5,16 +5,7 @@ import { REACT_APP_API_BASE_URL } from "./constants";
 
 export const api = createApi({
     reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-        baseUrl: REACT_APP_API_BASE_URL,
-        prepareHeaders: headers => {
-            headers.set(
-                "Authorization",
-                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1vZGVsZmFrQGdtYWlsLmNvbSIsInN1YiI6MSwicm9sZXMiOlsiYWRtaW4iXSwiaWF0IjoxNjg0ODE5NjI0LCJleHAiOjE2ODQ5MDYwMjR9.wksIpcaW5kJzvVxBsMjzNq-WrcOZmVkp1jXTFiS_xWM"
-            );
-            return headers;
-        },
-    }),
+    baseQuery: fetchBaseQuery({ baseUrl: REACT_APP_API_BASE_URL }),
     tagTypes: ["api"],
     endpoints: () => ({}),
     // eslint-disable-next-line consistent-return
