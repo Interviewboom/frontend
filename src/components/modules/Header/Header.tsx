bf74a2f11d31001d045707fc473375a62fbcbdf9
Volodymyr Gerun <vgerun97@gmail.com>
2023-05-29 21:32:49 +0300

    feat(Auth): added header layout to auth pages

--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -1,19 +1,29 @@
+import { useRouter } from "next/router";
+
 import { Logo } from "@elements/Logo";
 
 import { Auth } from "./Auth";
 import styles from "./Header.module.scss";
 import MobileMenu from "./MobileMenu";
 import { Navigation } from "./Navigation";
+import { AuthHeader } from "../AuthHeader";
 
 export const Header = () => {
+    const router = useRouter();
+    const isAuthPages = router.asPath.slice(1, 5) === "auth";
     return (
-        <header className={styles.header}>
-            <div className={styles.wrapper}>
-                <Logo redirectToHome />
-                <Navigation />
-                <Auth />
-            </div>
-            <MobileMenu />
-        </header>
+        <>
+            {isAuthPages && <AuthHeader logoComponent={<Logo redirectToHome />} />}
+            {!isAuthPages && (
+                <header className={styles.header}>
+                    <div className={styles.wrapper}>
+                        <Logo redirectToHome />
+                        {!isAuthPages && <Navigation />}
+                        {!isAuthPages && <Auth />}
+                    </div>
+                    <MobileMenu />
+                </header>
+            )}
+        </>
     );
 };
