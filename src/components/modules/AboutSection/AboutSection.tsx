578c570d774eeb013390d7b820742e62019d3c8c
Flowerinno <akellastoopi@gmail.com>
2023-06-01 14:01:01 +0300

    style: add flexbox model to about section

--- src/components/modules/AboutSection/AboutSection.tsx
+++ src/components/modules/AboutSection/AboutSection.tsx
@@ -9,31 +9,38 @@ export const AboutSection = () => {
     return (
         <section className={styles.section} id="about-us">
             <div className={styles.info}>
-                <Text isParagraph>Join us and we’ll take you to the next level.</Text>
-                <Title level={2}>
-                    Our platform offers a wide range of benefits for coders looking to test their skills. With
-                    comprehensive assessments, you can thoroughly evaluate your coding abilities and knowledge.
-                </Title>
-                <Text>
-                    A career showcase feature allows you to professionally showcase your expertise to potential
-                    employers, giving you a competitive advantage in a job market!
+                <Text isParagraph size="small" lineHeight={24} className={styles.infoLeftParagraph}>
+                    Join us and we’ll take you to the next level.
                 </Text>
-                <Text>
-                    The flexibility and convenience of our platform enable you to test skills at your own pace from
-                    anywhere and anytime. Additionally you can connect with the best community of coders for networking
-                    and of course learning opportunities.
-                </Text>
-                <Image
-                    src="/assets/images/about/aboutCodeSample.png"
-                    alt="IntreviewBoom about section code snippet"
-                    width={500}
-                    height={500}
-                />
+                <div className={styles.infoRight}>
+                    <Title level={2} className={styles.infoTitle}>
+                        Our platform offers a wide range of benefits for coders looking to test their skills. With
+                        comprehensive assessments, you can thoroughly evaluate your coding abilities and knowledge.
+                    </Title>
+                    <Text isParagraph size="small" lineHeight={17} className={styles.infoSmallParagraph}>
+                        A career showcase feature allows you to professionally showcase your expertise to potential
+                        employers, giving you a competitive advantage in a job market!
+                    </Text>
+                    <Text isParagraph lineHeight={24} className={styles.infoParagraph}>
+                        The flexibility and convenience of our platform enable you to test skills at your own pace from
+                        anywhere and anytime. Additionally you can connect with the best community of coders for
+                        networking and of course learning opportunities.
+                    </Text>
+                    <Image
+                        src="/assets/images/about/aboutCodeSample.png"
+                        alt="IntreviewBoom about section code snippet"
+                        className={styles.infoSampleImg}
+                        width={288}
+                        height={170}
+                    />
+                </div>
             </div>
             <div className={styles.journey}>
                 <div className={styles.journeyWrapper}>
                     <Text>InterviewBoom will help you</Text>
-                    <div className={styles.guide}>123</div>
+                    <div className={styles.guide}>
+
+                    </div>
                 </div>
             </div>
         </section>
